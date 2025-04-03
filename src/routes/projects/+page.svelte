c1ed3507b318893abdde2d74ee2684fb8826e540
Alessandra <alessandrabellosoares@gmail.com>
2025-04-03 12:31:34 -0300

    lab 6

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,5 +1,50 @@
+<script>
+  import projects from "$lib/projects.json";
+  import Project from "$lib/Project.svelte";
+  import Pie from '$lib/Pie.svelte';
+  import * as d3 from 'd3';
+
+  let rolledData = d3.rollups(projects, v => v.length, d => d.year);
+
+  let pieData;
+
+    $: {
+        // Initialize to an empty object every time this runs
+        pieData = {};
+        
+        // Calculate rolledData and pieData based on filteredProjects here
+        let rolledData = d3.rollups(filteredProjects, v => v.length, d => d.year);
+
+        // We don't need 'let' anymore since we already defined pieData
+        pieData = rolledData.map(([year, count]) => {
+            return { value: count, label: year };
+        });
+    }
+
+  let query = "";
+  $: filteredProjects = projects.filter(project => {
+    let values = Object.values(project).join("\n").toLowerCase();
+    return values.includes(query.toLowerCase());
+});
+
+</script>
+
 <svelte:head>
-  <title>Projetos</title>
+<title>Projects</title>
 </svelte:head>
 
-<h1>Projetos</h1>
+<h1>{ projects.length } Projects</h1>
+<Pie data={pieData} />
+<input type="search" bind:value={query} aria-label="Search projects" placeholder="🔍 Search projects..." />
+<div class="projects">
+  {#each filteredProjects as p}
+      <Project data={p}/>
+    <!-- <article>
+      <h2>{p.title}</h2>
+      <img src={p.image} alt="" />
+      <p>
+          {p.description}
+      </p>
+    </article> -->
+  {/each}
+</div>
